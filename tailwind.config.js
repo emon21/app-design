09ead5692eaca3dd9aef3354517db2ab928e723d
emon21 <emonbd66@gmail.com>
2023-01-12 17:58:06 +0600

    added

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {},
+    container: {
+        center: true,
+        padding: "0rem",
+        screens: {
+            sm: "600px",
+            md: "728px",
+            lg: "920px",
+            xl: "1024px",
+            // "2xl": "1320px",
+            "2xl": "450px"
+            
+        },
+    },
+  },
+  plugins: [],
+}
